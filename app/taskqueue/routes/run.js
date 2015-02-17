1447fc6c1cc7868c45d281330a8af7ef0c8153c5
mixa marciv <mixamarciv@gmail.com>
2015-02-17 17:10:41 +0300

    34

--- app/taskqueue/routes/run.js
+++ app/taskqueue/routes/run.js
@@ -16,19 +16,62 @@ module.exports = function(){
 
 
 function* run(next){
-    //var rows = yield f.db.gen_query(db_name,'SELECT * FROM task');
 
-    //var db = f.db.gen_connect(db_name);
+
     var p = this.request.query;
-    var q = 'SELECT idc,name,note FROM task WHERE 1=?';
-    var rows = yield f.db.gen_query('taskqueue',q,[1]);
     
+    {//проверяем все ли необходимые параметры заданы
+	var arr_check = ['name','note','script','cache_text','user_info'];
+	for(var i=0;i<arr_check.length;i++){
+	    var test = arr_check[i];
+	    if (!p[test]) {
+		this.body = {error:'не задан параметр "'+test+'"'};
+		return yield next;
+	    }
+	}
+    }
+
+    p.name;
+    p.note;
+    p.script;
+    p.cache_text;
+    p.cache_hash = g.crypto.createHash('sha1').update(p.cache_text).digest('hex');
+    p.user_info;
+    
+
+    var q = 'SELECT idc FROM task WHERE cache_hash=\''+p.cache_hash+'\'';
+    var rows = yield f.db.gen_query(db_name,q);
+    
+    if (rows.length==0) { // если такое задание ещё не выполнялось то запускаем его заново
+	return yield create_new_task(this,p); 
+    }
+    
+    //если такое уже было отправлено в очередь то обновляем информацию по количеству клиетов ожидающих ответа
+    var row = rows[0];
     
 
     //this.body = ;
-    this.body = g.mixa.dump.var_dump_node('a',{rows:rows,p:p,this:this},{dump_max_level:10,exclude: [/\.socket/i,/\._/i]});
+    this.body = g.mixa.dump.var_dump_node('a',{rows:rows,p:p},{dump_max_level:10,exclude: [/\.socket/i,/\._/i]});
 
 
     yield next;
 }
+
+function* create_new_task(self,p) {
+    ;
+    
+    var q = 'SELECT UUID_TO_CHAR( GEN_UUID() ) AS idt, UUID_TO_CHAR( GEN_UUID() ) AS idq FROM rdb$database';
+    var rows = yield f.db.gen_query(db_name,q);
+    var row = rows[0];
+    
+    var q = 'INSERT INTO task(idc,idc_firs_run,name,note,script,cache_text,cache_hash) '+
+	    'VALUES (?, ?, ?, ?, ?, ?, ?)';
+    yield f.db.gen_query(db_name, q, [row.idt, row.idq, p.name, p.note, p.script, p.cache_text, p.cache_hash]);
+    
+    var q = 'INSERT INTO task_queue(idc,idc_task,user_info) '+
+	    'VALUES (?, ?, ?)';
+    yield f.db.gen_query(db_name, q, [ row.idq, row.idt, p.user_info]);
+}
+
+
 
