b0cb506bb9fb8449ed03366df4a6290001bab474
mixa marciv <mixamarciv@gmail.com>
2015-02-20 10:26:56 +0300

    что то готово

--- app/taskqueue/db/create_database.bat
+++ app/taskqueue/db/create_database.bat
@@ -1,6 +1,6 @@
 @SET db_file_path=%~dp0
 @SET path_prefix=..\..\..
-@SET path_prefix=..\..\..
+@SET dbfilename=task.FDB
 
 @call "%path_prefix%\.bin\set_path.bat"
 @cd %db_file_path%
@@ -8,10 +8,10 @@
 @mkdir "%db_file_path%\%path_prefix%\temp\database"
 
 
-@echo CREATE DATABASE '%db_file_path%\app.fdb' page_size 8192 user 'SYSDBA' password 'masterkey'; > "%db_file_path%\%path_prefix%\temp\database\0001_create.sql"
+@echo CREATE DATABASE '%db_file_path%\%dbfilename%' page_size 8192 user 'SYSDBA' password 'masterkey'; > "%db_file_path%\%path_prefix%\temp\database\0001_create.sql"
 @isql -i "%db_file_path%\%path_prefix%\temp\database\0001_create.sql"
 
-@echo CONNECT '%db_file_path%\app.fdb' user 'SYSDBA' password 'masterkey'; > "%db_file_path%\%path_prefix%\temp\database\0002_connect.sql"
+@echo CONNECT '%db_file_path%\%dbfilename%' user 'SYSDBA' password 'masterkey'; > "%db_file_path%\%path_prefix%\temp\database\0002_connect.sql"
 
 @cd create_db_scripts
 @mkdir "%db_file_path%\%path_prefix%\temp\database\create_db_scripts"
